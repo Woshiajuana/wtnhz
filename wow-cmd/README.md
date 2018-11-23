967524d2c67bf2aa7ce608ae17f86256420bf274
Woshiajuana <zhigang.chen@owulia.com>
2018-11-23 14:37:12 +0800

    提交页面数据

---
+++ wow-cmd/README.md
@@ -0,0 +1,31 @@
+# wow-cmd
+
+> node cmd
+
+## Author
+
+> Ajuan <979703986@qq.com>
+
+## 目录结构
+
+```
+project
+├── lib                                     // 自带命令任务
+|   ├── template.cmd.js                     // 示例命令任务写法
+├── template                                // 自带命令任务模版
+|   ├── delete.cmd.js                       // 清除某个目录下文件任务模版
+├── test                                    // 测试任务
+|   ├── index.js
+├── utils                                   // 工具目录
+├── .babelrc                                // babel 配置文件
+├── .editorconfig                           // 代码格式文件
+├── .gitignore                              // 忽略文件
+├── index.js                                // 入口文件
+├── package.json
+├── package-lock.json
+├── README.md
+
+```
+
+
+
--- wow-cmd/index.js
+++ wow-cmd/index.js
@@ -1,59 +1,57 @@
 
-// import path from 'path'
-// import walk from './utils/walk.util'
-// import task from './utils/task.util'
-// import dupl from './utils/dupl.util'
-// import logs from './utils/log.util'
-//
-// const parameters = process.argv.splice(2);
-//
-// export default (opt = { cmdPath: '', options: { include: ['cmd.js'], exclude: [], }}) => {
-//     const libPath = path.join(__dirname, '/lib');
-//     let result = walk.run(libPath, {
-//         include: ['cmd.js'],
-//         exclude: [],
-//     });
-//     let {
-//         cmdPath,
-//         options,
-//     } = opt;
-//     if (cmdPath) {
-//         let cmdTask = walk.run(cmdPath, options);
-//         result = dupl.run(cmdTask, result);
-//     }
-//     if (!parameters.length)
-//         return;
-//     let fireFunArr = [];
-//     parameters.forEach((item, index) => {
-//         result.forEach((fireFun) => {
-//             if (typeof fireFun !== 'function'
-//                 || !fireFun.options
-//                 || !fireFun.options.cmd)
-//                 return null;
-//             let cmd = fireFun.options.cmd;
-//             let type = false;
-//             cmd.forEach((c) => {
-//                 if (item === c)
-//                     type = true;
-//             });
-//             if (!type)
-//                 return null;
-//             let options = {
-//                 params: parameters[index + 1],
-//                 parameters,
-//             };
-//             fireFunArr.push({
-//                 fireFun,
-//                 options,
-//             });
-//         })
-//     });
-//     if (!fireFunArr.length)
-//         return null;
-//     task.run(...fireFunArr);
-//     return null;
-// };
-//
-// export let log = logs;
+import path from 'path'
+import walk from './utils/walk.util'
+import task from './utils/task.util'
+import dupl from './utils/dupl.util'
+import logs from './utils/log.util'
 
-export default {}
+const parameters = process.argv.splice(2);
+
+export default (opt = { cmdPath: '', options: { include: ['cmd.js'], exclude: [], }}) => {
+    const libPath = path.join(__dirname, '/lib');
+    let result = walk.run(libPath, {
+        include: ['cmd.js'],
+        exclude: [],
+    });
+    let {
+        cmdPath,
+        options,
+    } = opt;
+    if (cmdPath) {
+        let cmdTask = walk.run(cmdPath, options);
+        result = dupl.run(cmdTask, result);
+    }
+    if (!parameters.length)
+        return;
+    let fireFunArr = [];
+    parameters.forEach((item, index) => {
+        result.forEach((fireFun) => {
+            if (typeof fireFun !== 'function'
+                || !fireFun.options
+                || !fireFun.options.cmd)
+                return null;
+            let cmd = fireFun.options.cmd;
+            let type = false;
+            cmd.forEach((c) => {
+                if (item === c)
+                    type = true;
+            });
+            if (!type)
+                return null;
+            let options = {
+                params: parameters[index + 1],
+                parameters,
+            };
+            fireFunArr.push({
+                fireFun,
+                options,
+            });
+        })
+    });
+    if (!fireFunArr.length)
+        return null;
+    task.run(...fireFunArr);
+    return null;
+};
+
+export let log = logs;
