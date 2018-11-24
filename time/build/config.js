c6b14ebef6ea9fd225c0b9fdb17ced47274bc60b
Woshiajuana <zhigang.chen@owulia.com>
2018-11-24 20:29:44 +0800

    提交更新页面数据

--- time/build/config.js
+++ time/build/config.js
@@ -1,5 +1,7 @@
+const path = require('path');
+
 module.exports = {
 
     // 目录
-    
+    viewDir: path.join(__dirname, '../src/views')
 };
--- time/build/webpack.config.js
+++ time/build/webpack.config.js
@@ -4,19 +4,23 @@ const path = require('path');
 const fs = require('fs');
 const config = require('./config');
 
+let {
+    viewDir,
+} = config;
+
 // 遍历目录结构
 let walkFun;
 (walkFun = (dir) => {
     dir = dir || '.';
-    let directory = path.join(__dirname, '../src/views', dir);
+    let directory = path.join(viewDir, dir);
+    console.log(directory)
     fs.readdirSync(directory).forEach((file) => {
-
+        
     })
 })();
 
 const entry = {};
-const config = {
+return;
+module.exports = {
 
 };
-
-module.exports = config;
