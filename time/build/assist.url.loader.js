5b0fa436371d2ac92718d1a78ce2af755142b966
Woshiajuana <zhigang.chen@owulia.com>
2018-11-25 17:33:19 +0800

    提交更新

--- time/build/assist.url.loader.js
+++ time/build/assist.url.loader.js
@@ -1,10 +1,7 @@
-let Path = require('path');
-let fs = require('fs');
 let _loaderUtils = require('loader-utils');
 
 module.exports = function (content) {
     let options = (0, _loaderUtils.getOptions)(this) || {};
-    // console.log(this.resource.indexOf('\\html\\') > 0)
     let {
         urlLoader,
         html,
