06e1c486e5e39e1f8bd61c281d8e19bc82374754
Woshiajuana <zhigang.chen@owulia.com>
2018-11-26 10:25:29 +0800

    提交更新页面数据

--- time/build/walk.util.js
+++ time/build/walk.util.js
@@ -2,20 +2,24 @@
 const fs = require('fs');
 const path = require('path');
 
-let result = {};
+
 module.exports = {
     run (dirPath) {
-        fs.readdirSync(dirPath).forEach((file) => {
-            let filePath = path.join(dirPath, '/' + file);
-            let fileStat = fs.statSync(filePath);
-            if (fileStat.isFile() && file === 'entry.js') {
-                let fileDirArr = filePath.replace(/\\/g, '/').split('\/');
-                let key = fileDirArr[fileDirArr.length - 2];
-                result[key] = filePath;
-            } else if (fileStat.isDirectory()) {
-                this.run(filePath);
-            }
-        });
+        let result = {};
+        let loop;
+        (loop = (dir) => {
+            fs.readdirSync(dir).forEach((file) => {
+                let filePath = path.join(dir, '/' + file);
+                let fileStat = fs.statSync(filePath);
+                if (fileStat.isFile() && file === 'entry.js') {
+                    let fileDirArr = filePath.replace(/\\/g, '/').split('\/');
+                    let key = fileDirArr[fileDirArr.length - 2];
+                    result[key] = filePath;
+                } else if (fileStat.isDirectory()) {
+                    loop(filePath);
+                }
+            });
+        })(dirPath);
         return result;
     }
 };
--- wow-cmd/utils/walk.util.js
+++ wow-cmd/utils/walk.util.js
@@ -14,28 +14,31 @@ module.exports = {
             include,
             exclude,
         } = options;
-        try {
-            let files = fs.readdirSync(cmdPath);
-            files.forEach((file) => {
-                let filePath = path.join(cmdPath, '/' + file);
-                let fileStat = fs.statSync(filePath);
-                if (fileStat.isFile() && indexOf(include, file)) {
-                    try {
-                        let fireFun = require(filePath);
-                        if (typeof fireFun !== 'function')
-                            fireFun = fireFun.default;
-                        if (typeof fireFun === 'function')
-                            result.push(fireFun);
-                    } catch (e) {
-                        console.error('导入' + filePath +'文件错误',e);
+        let loop;
+        (loop = (dir) => {
+            try {
+                let files = fs.readdirSync(dir);
+                files.forEach((file) => {
+                    let filePath = path.join(dir, '/' + file);
+                    let fileStat = fs.statSync(filePath);
+                    if (fileStat.isFile() && indexOf(include, file)) {
+                        try {
+                            let fireFun = require(filePath);
+                            if (typeof fireFun !== 'function')
+                                fireFun = fireFun.default;
+                            if (typeof fireFun === 'function')
+                                result.push(fireFun);
+                        } catch (e) {
+                            console.error('导入' + filePath +'文件错误',e);
+                        }
+                    } else if (fileStat.isDirectory() && indexOf(exclude, file)) {
+                        loop(filePath);
                     }
-                } else if (fileStat.isDirectory() && indexOf(exclude, file)) {
-                    this.run(filePath);
-                }
-            })
-        } catch (e) {
-            console.error('遍历CMD任务出现错误：',e);
-        }
+                })
+            } catch (e) {
+                console.error('遍历CMD任务出现错误：',e);
+            }
+        })(cmdPath);
         return result;
     }
 };
