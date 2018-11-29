dd4c7bf072c880f0c40db0b2dc1ae8c624244dea
Woshiajuana <zhigang.chen@owulia.com>
2018-11-29 14:58:34 +0800

    提交

--- time/src/views/index/index.js
+++ time/src/views/index/index.js
@@ -1,18 +1,19 @@
 import $ from 'jquery'
-// import Swiper from 'swiper'
-import jm from 'jquery-mousewheel'
-jm($)
-console.log(jm)
+import $jm from 'assets/lib/jquery.mousewheel'
+$jm($);
 
 const IMAGE_ARR = [
     {
-        "url": "http://dummyimage.com/1920x1080/79d2f2"
+        "url": "http://dummyimage.com/1920x1080/79d2f2",
+        "page": "1",
     },
     {
-        "url": "http://dummyimage.com/1920x1080/f2ee79"
+        "url": "http://dummyimage.com/1920x1080/f2ee79",
+        "page": "2",
     },
     {
-        "url": "http://dummyimage.com/1920x1080/ca79f2"
+        "url": "http://dummyimage.com/1920x1080/ca79f2",
+        "page": "3",
     },
 ];
 
@@ -24,6 +25,32 @@ const BannerController = {
     addEvent () {
         console.log(2)
         $('.main-wrap').on('mousewheel', this.handleScroll.bind(this));
+
+        // 点击浏览器的前进后退按钮处理
+        window.onpopstate = function (e) {
+            console.log(e);
+            if (e.state) {
+                document.title = e.state.title;
+                $.ajax({
+                    type: "get",
+                    url: e.state.url,
+                    success: function (data) {
+                        $("#page").html(data)
+                    },
+                    error: function (data) {
+
+                    }
+                })
+            }
+        }
+        //处理点浏览器返回时候最后一个不刷新页面内容问题
+        var state = {
+            title: document.title,
+            url: document.location.href,
+            otherkey: null
+        };
+        history.replaceState(state, document.title, document.location.href);
+
     },
     handleScroll (e) {
         console.log(e)
