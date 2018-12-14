8702ecc72c1b860862c4d5c7805f29b9a69d7351
Woshiajuana <zhigang.chen@owulia.com>
2018-12-14 10:45:00 +0800

    提交更新页面数据

---
+++ weex-app/src/config/wow-weex-plugin.config.js
@@ -0,0 +1,8 @@
+import AppConfig                from 'appConfig/app.config'
+
+const { app } = AppConfig;
+
+export default {
+    APP: app,
+    BAR_COLOR: '#5cc8ff',
+}
---
+++ weex-app/src/views/wow/demo/demo.meta.json.js
@@ -0,0 +1,71 @@
+export default {
+    data: {
+        arr_grid: [
+            {
+                src: 'https://img.mukewang.com/5b67ab77000123c409360316.jpg',
+                text: 'HELP1',
+                key: 'wow_demo_list',
+            },
+            {
+                src: 'https://img.mukewang.com/5b67ab77000123c409360316.jpg',
+                text: 'HELP2',
+                key: 'wow_demo_list',
+            },
+            {
+                src: 'https://img.mukewang.com/5b67ab77000123c409360316.jpg',
+                text: 'HELP3',
+                key: 'wow_demo_list',
+            },
+            {
+                src: 'https://img.mukewang.com/5b67ab77000123c409360316.jpg',
+                text: 'HELP4',
+                key: 'wow_demo_list',
+            },
+            {
+                src: 'https://img.mukewang.com/5b67ab77000123c409360316.jpg',
+                text: 'HELP2',
+                key: 'wow_demo_list',
+            },
+            {
+                src: 'https://img.mukewang.com/5b67ab77000123c409360316.jpg',
+                text: 'HELP3',
+                key: 'wow_demo_list',
+            },
+            {
+                src: 'https://img.mukewang.com/5b67ab77000123c409360316.jpg',
+                text: 'HELP4',
+                key: 'wow_demo_list',
+            },
+            {
+                src: 'https://img.mukewang.com/5b67ab77000123c409360316.jpg',
+                text: 'HELP2',
+                key: 'wow_demo_list',
+            },
+            {
+                src: 'https://img.mukewang.com/5b67ab77000123c409360316.jpg',
+                text: 'HELP3',
+                key: 'wow_demo_list',
+            },
+            {
+                src: 'https://img.mukewang.com/5b67ab77000123c409360316.jpg',
+                text: 'HELP4',
+                key: 'wow_demo_list',
+            },
+            {
+                src: 'https://img.mukewang.com/5b67ab77000123c409360316.jpg',
+                text: 'HELP2',
+                key: 'wow_demo_list',
+            },
+            {
+                src: 'https://img.mukewang.com/5b67ab77000123c409360316.jpg',
+                text: 'HELP3',
+                key: 'wow_demo_list',
+            },
+            {
+                src: 'https://img.mukewang.com/5b67ab77000123c409360316.jpg',
+                text: 'HELP4',
+                key: 'wow_demo_list',
+            }
+        ]
+    }
+}
