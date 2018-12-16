65471ec04fb17e465a0b0734861303da32a8686b
Woshiajuana <zhigang.chen@owulia.com>
2018-12-16 17:01:10 +0800

    提交更新

--- back-services/src/controllers/question.app.controller.js
+++ back-services/src/controllers/question.app.controller.js
@@ -1,3 +1,13 @@
-export default {
 
+class Controller {
+    async hello (ctx, next) {
+        console.log(2)
+        ctx.pipeDone()
+    }
 }
+
+export const routes = {
+    
+};
+
+export default new Controller();
--- back-services/src/routes/index.js
+++ back-services/src/routes/index.js
@@ -1,19 +1,26 @@
 
 import path                     from 'path'
 import requireAll               from 'require-all'
+import Router                   from 'koa-router'
+
+const router = new Router();
 
 const controllers = requireAll({
     dirname: path.join(__dirname, '../controllers/'),
     filter: /(.+)\.controller\.js$/,
 });
 
-const routers = requireAll({
+const routes = requireAll({
     dirname: path.join(__dirname, './'),
     filter: /(.+)\.route\.js$/,
 });
 
 console.log(controllers);
-console.log(routers);
+console.log(routes);
+
+
+export default router;
+
 
 // import UserAppRoute             from './user.app.route'
 //
