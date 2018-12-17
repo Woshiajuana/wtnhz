53da5d91921353cb2c4616feec146d366dcffd3f
Woshiajuana <zhigang.chen@owulia.com>
2018-12-17 11:38:52 +0800

    提交更新

--- back-services/src/app.js
+++ back-services/src/app.js
@@ -6,5 +6,10 @@ import router               from './router'
 
 const app = new Koa();
 
+// 路由
+app.use(router.routes());
+app.use(router.allowedMethods({
+    throw: true,
+}));
 
 export default app;
--- back-services/src/controllers/question.app.controller.js
+++ back-services/src/controllers/question.app.controller.js
@@ -2,9 +2,11 @@
 class Controller {
     async hello (ctx, next) {
         console.log('hello')
+        next();
     }
     async test (ctx, next) {
         console.log('test')
+        next();
     }
 }
 
