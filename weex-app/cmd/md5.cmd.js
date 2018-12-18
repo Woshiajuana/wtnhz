886b4284d2cd388f8964c485a91dac021d064259
Woshiajuana <zhigang.chen@owulia.com>
2018-12-18 15:38:23 +0800

    提交

--- weex-app/cmd/md5.cmd.js
+++ weex-app/cmd/md5.cmd.js
@@ -46,28 +46,3 @@ Handle.error = (err, next) => {
 };
 
 export default Handle;
-
-
-export default((arr_parameter) => new Promise((resolve, reject) => {
-    try {
-        let num_ip_index = wowCool.findFirstIndexForArr(arr_parameter, (item) => {
-            return item === '--md5' || item === '-m';
-        });
-        if (num_ip_index === -1) return resolve();
-        let tree = fsTool.readFileSync(path.join(__dirname, `./../dist/${env}/tree.json`));
-        tree = JSON.parse(tree);
-        _.forEach(tree.resource, (source, key) => {
-            const filename = source.src;
-            console.log(path.join(__dirname, `./../dist/${env}/${filename}`));
-            const file = fsTool.readFileSync(path.join(__dirname, `./../dist/${env}/${filename}`));
-            source.md5 = crypto.createHash('md5').update(file).digest('hex');
-            log(`${filename}md5值 => ${ source.md5 }`);
-        });
-        fsTool.writeFileSync(`./dist/${env}/tree.json`, JSON.stringify(tree, null, 4));
-        log('md5计算完成');
-        return resolve();
-    } catch (err) {
-        console.log(err);
-        resolve();
-    }
-}));
