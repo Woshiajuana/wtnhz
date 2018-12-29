e88c2867297f1c6fb8ccf25e63e8141e39936a0a
Woshiajuana <zhigang.chen@owulia.com>
2018-12-29 14:37:17 +0800

    提交更新页面数据

--- back-services/src/controllers/user.app.controller.js
+++ back-services/src/controllers/user.app.controller.js
@@ -1,3 +1,17 @@
-export default {
 
+class Controller {
+    async login (ctx, next) {
+        console.log('login')
+        next();
+    }
+    async send (ctx, next) {
+        console.log('send')
+        next();
+    }
+    async register (ctx, next) {
+        console.log('register')
+        next();
+    }
 }
+
+export default new Controller();
--- back-services/src/router/routes.js
+++ back-services/src/router/routes.js
@@ -17,7 +17,45 @@ export default [
             ],
         },
         children: [
-
+            // /user
+            {
+                path: '/user',
+                children: [
+                    {
+                        path: '/login',
+                        request: {
+                            post: [
+                                {
+                                    controller: 'user.app',
+                                    method: 'login',
+                                },
+                            ],
+                        },
+                    },
+                    {
+                        path: '/send',
+                        request: {
+                            post: [
+                                {
+                                    controller: 'user.app',
+                                    method: 'send',
+                                },
+                            ]
+                        }
+                    },
+                    {
+                        path: '/register',
+                        request: {
+                            post: [
+                                {
+                                    controller: 'user.app',
+                                    method: 'register',
+                                },
+                            ],
+                        },
+                    },
+                ]
+            }
         ]
     }
 ]
