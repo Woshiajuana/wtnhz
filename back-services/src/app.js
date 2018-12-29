6d59d68193a002403086808ecd9174a3e20cd48e
Woshiajuana <zhigang.chen@owulia.com>
2018-12-29 15:42:22 +0800

    提交更新页面数据

--- back-services/src/app.js
+++ back-services/src/app.js
@@ -1,15 +1,46 @@
 
 import Koa                  from 'koa'
-import koaLogger            from 'koa-logger'
 import koaSession           from 'koa-session'
+import koaConvert           from 'koa-convert'
+import koaBody              from 'koa-body'
 import router               from './router'
+import loggerUtil           from './utils/logger.util'
+import handleMid            from './middlewares/handle.middleware'
 
 const app = new Koa();
+app.jsonSpaces = 0; // 压缩json返回中的空格
+app.keys = ['key'];
+
+// 请求解析
+app.use(koaConvert(koaBody({
+    multipart: true,
+    formLimit: '5mb',
+})));
+
+// 中间件
+app.use(koaConvert.compose(
+    handleMid() // 处理者
+));
+
+// 日志
+app.use(loggerUtil.http());
+// 日志
+app.use(async (ctx, next) => {
+    const start = new Date();
+    await next();
+    const ms = new Date() - start;
+    loggerUtil.app().info(`${ctx.method} ${ctx.url} - ${ms}ms`)
+});
 
 // 路由
 app.use(router.routes());
 app.use(router.allowedMethods({
     throw: true,
 }));
+
+// 监听错误
+app.on('error', (err, ctx) => {
+    loggerUtil.app().error('服务错误: ', err, ctx)
+});
 
 export default app;
---
+++ back-services/src/middlewares/handle.middleware.js
@@ -0,0 +1,33 @@
+
+import _                    from 'lodash'
+import logger               from './../utils/logger.util'
+import { loggerType }       from './../config/logger.config'
+
+export default () => async (ctx, next) => {
+    try {
+        ctx._pipeDoneData = {};
+        ctx._pipeFailData = {};
+        ctx.pipeDone = (result) => {
+            ctx._pipeDoneData = { code: '0000', result };
+        };
+        ctx.pipeFail = (input, code = '9999') => {
+            const message = _.get(input, 'message') || input;
+            const stack = _.get(input, 'stack') || undefined;
+            ctx._pipeFailData = { code, message };
+            const errorType = _.includes(loggerType, _.get(input, 'type')) ? input.type : 'system';
+            logger[errorType]().error(__dirname, '失败原因: ', stack || message)
+        };
+        await next();
+        // 拦截返回
+        if (!_.isEmpty(ctx._pipeFailData)) {
+            ctx.body = ctx._pipeFailData;
+            return null
+        }
+        if (!_.isEmpty(ctx._pipeDoneData)) {
+            ctx.body = ctx._pipeDoneData;
+            return null
+        }
+    } catch (err) {
+        ctx.app.emit('error', err, ctx);
+    }
+}
