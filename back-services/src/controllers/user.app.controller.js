0a91feb42a0469dcf9640da88f8d422426327c75
Woshiajuana <zhigang.chen@owulia.com>
2018-12-31 21:31:41 +0800

    提交更新页面数据

--- back-services/src/controllers/user.app.controller.js
+++ back-services/src/controllers/user.app.controller.js
@@ -4,15 +4,11 @@ import MailService          from '../services/mail.service'
 import UserService          from '../services/user.service'
 
 class Controller {
+
+    // 登录
     async login (ctx, next) {
-        console.log('login')
-        next();
-    }
-    async send (ctx, next) {
         try {
-            let {
-                email
-            } = await ctx.check$.testBody((check) => {
+            let filterParams = await ctx.check$.testBody((regular) => {
                 return {
                     email: [
                         {
@@ -20,26 +16,69 @@ class Controller {
                             prompt: '缺少必要参数',
                         },
                         {
-                            rule: check.isEmail,
+                            rule: regular.isEmail,
+                            prompt: '参数格式错误',
+                        },
+                    ],
+                    password: [
+                        {
+                            nonempty: true,
+                            prompt: '缺少必要参数',
+                        },
+                        {
+                            rule: (value) => {
+                                let len = value.length;
+                                return len >= 6 && len <= 32;
+                            },
+                            prompt: '密码长度为6~32位',
+                        },
+                    ],
+                }
+            });
+            let user = await UserService.one(filterParams);
+            if (!user)
+                throw '账号密码错误';
+            ctx.handle$.success(user);
+        } catch (err) {
+            ctx.handle$.error(err);
+        }
+    }
+
+    // 发送验证码
+    async send (ctx, next) {
+        try {
+            let {
+                email
+            } = await ctx.check$.testBody((regular) => {
+                return {
+                    email: [
+                        {
+                            nonempty: true,
+                            prompt: '缺少必要参数',
+                        },
+                        {
+                            rule: regular.isEmail,
                             prompt: '参数格式错误',
                         },
                     ]
                 }
             });
             let code = await CodeService.get(email);
-            await MailService.send(email, '注册验证码', `验证码为${code}`);
-            ctx.handle$.success({}, '发送验证码成功');
+            await MailService.send(email, 'WTNHZ', `您好，您的验证码为${code}，有效期为十分钟哦~`);
+            ctx.handle$.success();
         } catch (err) {
             ctx.handle$.error(err);
         }
     }
+
+    // 注册
     async register (ctx, next) {
         try {
             let filterParams;
             let {
                 email,
                 code,
-            } = filterParams = await ctx.check$.testBody((check) => {
+            } = filterParams = await ctx.check$.testBody((regular) => {
                 return {
                     email: [
                         {
@@ -47,7 +86,7 @@ class Controller {
                             prompt: '缺少必要参数',
                         },
                         {
-                            rule: check.isEmail,
+                            rule: regular.isEmail,
                             prompt: '参数格式错误',
                         },
                     ],
@@ -73,8 +112,11 @@ class Controller {
                 }
             });
             await CodeService.check(email, code);
+            let user = await UserService.one(filterParams);
+            if (user)
+                throw '该邮箱已注册';
             await UserService.create(filterParams);
-            ctx.handle$.success({}, '注册成功');
+            ctx.handle$.success();
         } catch (err) {
             ctx.handle$.error(err);
         }
