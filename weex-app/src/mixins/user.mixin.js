9d5078c572e8794ed2155b893a2636478092cba7
Woshiajuana <zhigang.chen@owulia.com>
2019-01-10 17:33:25 +0800

    提交更新页面数据

--- weex-app/src/mixins/user.mixin.js
+++ weex-app/src/mixins/user.mixin.js
@@ -1,10 +1,5 @@
 
-import StorePlugin                      from 'plugins/store.plugin'
-import ModalPlugin                      from 'plugins/modal.plugin'
-
-const AUTH_KEY_NAME = 'AUTH_KEY_NAME';
-const AUTH_USER_STORE_KEY_NAME = 'AUTH_USER_STORE_KEY_NAME';
-const AUTH_USER_CACHE_KEY_NAME = 'AUTH_USER_CACHE_KEY_NAME';
+import UserService                      from 'services/user.service'
 
 const data = () => {
     return {
@@ -16,49 +11,14 @@ const methods = {
     user$Get () {
         let that = this;
         return new Promise((resolve, reject) => {
-            StorePlugin.get(AUTH_KEY_NAME).then((info) => {
+            UserService.get().then((info) => {
                 that.user$ = info;
                 resolve(info);
             }).catch((err) => {
                 that.user$ = '';
-                reject('用户未登录');
+                reject(err);
             })
         });
-    },
-    user$Set (info) {
-        let that = this;
-        return new Promise((resolve, reject) => {
-            StorePlugin.set(AUTH_KEY_NAME, info).then(() => {
-                that.user$ = info;
-                resolve(info);
-            }).catch(() => {
-                that.user$ = '';
-                reject('存储失败');
-            });
-        });
-    },
-    user$Upt (input) {
-        let info = {};
-        return this.user$Get().then((res) => {
-            info = Object.assign(info, res, input);
-        }).catch(() => {
-            info = input;
-        }).finally(() => {
-            return this.user$Set(info);
-        });
-    },
-    user$Logout () {
-        return new Promise((resolve, reject) => {
-            Promise.all([
-                StorePlugin.remove(AUTH_KEY_NAME),
-                StorePlugin.remove(AUTH_USER_CACHE_KEY_NAME),
-            ]).then(() => {
-                ModalPlugin.show('wow_user_login');
-                resolve();
-            }).catch((err) => {
-                reject('缓存清除失败');
-            })
-        })
     },
 };
 
