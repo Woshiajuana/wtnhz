d7a9ed5b56f88de2e1ec31d93510b9c52f888b75
Woshiajuana <zhigang.chen@owulia.com>
2019-01-24 13:52:50 +0800

    提交更新页面数据

--- back-services/src/models/follower.model.js
+++ back-services/src/models/follower.model.js
@@ -6,12 +6,25 @@ import mongoose                 from 'mongoose'
 const Schema = new mongoose.Schema({
 
     // 被关注的人
-    user: {
+    following: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User' //这里要写你指向的数据库表名字
     },
 
-    
+    // 关注的人
+    follower: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User' //这里要写你指向的数据库表名字
+    },
+
+    // 创建信息
+    create: {
+        // 时间
+        date: {
+            type: Date,
+            default: Date.now,
+        },
+    },
 
 });
 
