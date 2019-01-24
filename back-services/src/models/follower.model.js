fe725b618cb1e9fb5107363cfe99487223a84777
Woshiajuana <zhigang.chen@owulia.com>
2019-01-24 13:44:07 +0800

    提交更新页面数据

--- back-services/src/models/follower.model.js
+++ back-services/src/models/follower.model.js
@@ -3,3 +3,16 @@
 
 import mongoose                 from 'mongoose'
 
+const Schema = new mongoose.Schema({
+
+    // 被关注的人
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User' //这里要写你指向的数据库表名字
+    },
+
+    
+
+});
+
+export default mongoose.model('Follower', Schema);
---
+++ back-services/src/models/following.model.js
@@ -0,0 +1,10 @@
+
+// 关注表
+
+import mongoose                 from 'mongoose'
+
+const Schema = new mongoose.Schema({
+
+});
+
+export default mongoose.model('Following', Schema);
