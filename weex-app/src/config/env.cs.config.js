928a891532abea09e03559a6fcbbc2d7b8a6ead5
Woshiajuana <zhigang.chen@owulia.com>
2019-01-25 13:17:08 +0800

    提交根性页面庶几乎

--- weex-app/src/config/env.cs.config.js
+++ weex-app/src/config/env.cs.config.js
@@ -1,5 +1,4 @@
-import BD_IP from './../../cmd/config/ip.config'
 export default {
-    PICTURE_URL: 'http://'+ BD_IP +':32580/static/images/',             // 图片服务器
+    PICTURE_URL: 'http://47.95.202.71:32580/static/images/',             // 图片服务器
     API_URL: 'http://47.95.202.71:10010',                               // 接口
 }
