1b7b2f35a362e1f23179ddcc7f7982eb81a540e7
Woshiajuana <zhigang.chen@owulia.com>
2019-01-25 10:57:22 +0800

    提交更新页面数据

--- weex-app/src/config/env.bd.config.js
+++ weex-app/src/config/env.bd.config.js
@@ -1,8 +1,5 @@
 import BD_IP from './../../cmd/config/ip.config'
 export default {
     PICTURE_URL: 'http://'+ BD_IP +':32580/static/images/',             // 图片服务器
-    HTML_URL:'http://'+ BD_IP +':32580/static/html/',                   // 静态页面
-    JSON_URL: 'http://'+ BD_IP +':32580/static/json/',                  // JSON文件地址
-    // API_URL: 'http://rap2api.taobao.org/app/mock/116929',        // 接口
     API_URL: 'http://'+ BD_IP +':10010',        // 接口
 }
--- weex-app/src/config/env.cs.config.js
+++ weex-app/src/config/env.cs.config.js
@@ -1,7 +1,5 @@
+import BD_IP from './../../cmd/config/ip.config'
 export default {
-    PICTURE_URL: 'http://www.owulia.com/market/static/images/',         // 图片服务器
-    HTML_URL:'http://www.owulia.com/market/static/html/',               // 静态页面
-    JSON_URL: 'http://www.owulia.com/market/static/json/',              // JSON文件地址
-    API_URL: 'http://192.168.2.52:8082/exchange/openapi/v1/app',        // 接口
-    OLD_API_URL: 'http://192.168.2.52:8082'                             // 老接口
+    PICTURE_URL: 'http://'+ BD_IP +':32580/static/images/',             // 图片服务器
+    API_URL: 'http://47.95.202.71:10010',                               // 接口
 }
