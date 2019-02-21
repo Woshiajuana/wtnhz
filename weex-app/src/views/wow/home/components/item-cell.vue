413e35168d8964b20eedfd5f6005118e8c7c8cba
Woshiajuana <zhigang.chen@owulia.com>
2019-02-21 17:55:47 +0800

    提交

---
+++ weex-app/src/views/wow/home/components/item-cell.vue
@@ -0,0 +1,96 @@
+<template>
+    <div class="wrap">
+        <div class="inner">
+            <image class="image" src="https://img.mukewang.com/5c6d3e4e0001946418720632.jpg"></image>
+            <div class="content">
+                <div class="title">
+                    <text class="title-text">javascript基础开发</text>
+                </div>
+                <div class="type">
+                    <div class="type-item">
+                        <text class="type-text">JAVA</text>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="info">
+            <div class="info-item">
+                <image class="info-icon"></image>
+                <text class="info-text">999</text>
+            </div>
+            <div class="info-item">
+                <image class="info-icon"></image>
+                <text class="info-text">999</text>
+            </div>
+            <div class="info-item">
+                <image class="info-icon"></image>
+                <text class="info-text">999</text>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+    .wrap{
+        padding-left: 32px;
+        background-color: #fff;
+    }
+    .image{
+        width: 140px;
+        height: 140px;
+        margin-right: 32px;
+    }
+    .inner{
+        flex-direction: row;
+        align-items: center;
+        padding-right: 32px;
+        flex: 1;
+        padding-top: 32px;
+        padding-bottom: 0;
+    }
+    .title{
+
+    }
+    .title-text{
+
+    }
+    .type{
+
+    }
+    .type-item{
+
+    }
+    .type-text{
+
+    }
+    .info{
+        flex-direction: row;
+        align-items: center;
+        height: 80px;
+        border-top-width: 1px;
+        border-top-color: #dedede;
+        border-bottom-width: 1px;
+        border-bottom-color: #dedede;
+    }
+    .info-item{
+        justify-content: center;
+        align-items: center;
+        flex: 1;
+        height: 40px;
+        background-color: #fff;
+        border-right-width: 1px;
+        border-color: #dedede;
+    }
+    .info-icon{
+
+    }
+    .info-text{
+
+    }
+</style>
--- weex-app/src/views/wow/home/home.vue
+++ weex-app/src/views/wow/home/home.vue
@@ -1,10 +1,17 @@
 <template>
     <wow-view
+        view_use_scroll=""
         view_use_header=""
         view_header_left_src="">
         <head-section
             slot="view-header"
         ></head-section>
+        <scroller class="list">
+            <item-cell></item-cell>
+            <item-cell></item-cell>
+            <item-cell></item-cell>
+            <item-cell></item-cell>
+        </scroller>
     </wow-view>
 </template>
 
@@ -12,6 +19,7 @@
     import WowView                      from 'wow-weex-ui/lib/wow-view'
     import SourceMixin                  from 'mixins/source.mixin'
     import HeadSection                  from './components/head-section.vue'
+    import ItemCell                     from './components/item-cell.vue'
 
     const srcArr = [
         { key: 'arrow', value: 'triangle-down-ffffff-icon.png', },
@@ -27,11 +35,14 @@
         },
         components: {
             WowView,
+            ItemCell,
             HeadSection,
         },
     }
 </script>
 
 <style>
-
+    .list{
+        flex: 1;
+    }
 </style>
