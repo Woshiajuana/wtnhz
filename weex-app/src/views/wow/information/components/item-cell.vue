c4656a2c255181fc195dcd5dcc59abff28f9bbcd
Woshiajuana <zhigang.chen@owulia.com>
2019-03-01 15:57:50 +0800

    提交

---
+++ weex-app/src/views/wow/information/components/item-cell.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="wrap">
+        <image class="image"></image>
+        <div class="inner">
+            <div class="section">
+                <text class="label name">系统通知</text>
+                <text class="value time">2018-03-03</text>
+            </div>
+            <div class="section">
+                <text class="label prompt">哈哈哈哈哈啊哈哈哈哈</text>
+                <text class="value type">99</text>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+    .wrap{
+
+    }
+    .image{
+
+    }
+    .inner{
+
+    }
+    .section{
+
+    }
+    .label{
+
+    }
+    .value{
+
+    }
+    .name{
+
+    }
+    .time{
+
+    }
+    .prompt{
+
+    }
+    .type{
+
+    }
+</style>
--- weex-app/src/views/wow/information/information.vue
+++ weex-app/src/views/wow/information/information.vue
@@ -1,17 +1,45 @@
 <template>
-    <wow-view>
-        <text>22</text>
+    <wow-view
+        view_use_scroll=""
+        view_header_left_src=""
+        :view_style="{backgroundColor: '#f2f2f2'}"
+        :view_header_wrap_style="{backgroundColor: '#fc5366'}"
+        :view_header_center_txt_style="{color: '#fff'}"
+        view_header_center_txt="消息">
+        <wow-scroll
+            @refresh="handleRefresh"
+            @loading="handleLoading">
+            <item-cell></item-cell>
+            <item-cell></item-cell>
+            <item-cell></item-cell>
+            <item-cell></item-cell>
+            <item-cell></item-cell>
+            <item-cell></item-cell>
+            <item-cell></item-cell>
+            <item-cell></item-cell>
+            <item-cell></item-cell>
+        </wow-scroll>
     </wow-view>
 </template>
 
 <script>
     import WowView                      from 'wow-weex-ui/lib/wow-view'
-
-
+    import WowScroll                    from 'wow-weex-ui/lib/wow-scroll'
+    import ItemCell                     from './components/item-cell.vue'
 
     export default {
+        methods: {
+            handleRefresh (callback) {
+                callback && callback();
+            },
+            handleLoading (callback) {
+                callback && callback();
+            },
+        },
         components: {
             WowView,
+            WowScroll,
+            ItemCell,
         },
     }
 </script>
