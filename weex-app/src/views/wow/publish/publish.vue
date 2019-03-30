5968f56c87cf3f273441c9767e8414c5218fc28a
Woshiajuana <zhigang.chen@owulia.com>
2019-03-30 21:19:46 +0800

    提交

--- weex-app/src/views/wow/publish/publish.vue
+++ weex-app/src/views/wow/publish/publish.vue
@@ -1,17 +1,27 @@
 <template>
-    <wow-view>
+    <wow-view
+        :view_header_left_src="src$.close">
 
     </wow-view>
 </template>
 
 <script>
     import WowView                      from 'wow-weex-ui/lib/wow-view'
+    import SourceMixin                  from 'mixins/source.mixin'
 
-
+    const srcArr = [
+        { key: 'close', value: 'header-left-close.png?6', },
+    ];
 
     export default {
+        mixins: [
+            SourceMixin,
+        ],
         components: {
             WowView,
+        },
+        created () {
+            this.sourceGet(srcArr);
         },
     }
 </script>
